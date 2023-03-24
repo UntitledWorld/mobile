3b3a2b728999a85f6d326ccdf4ec3e9b9b98a37a
Lee <leejavaa@gmail.com>
2023-03-24 07:57:40 +0200

    UNT-1-Setup basic navigation

---
+++ screens/ScanInput.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ScanInput = () => {
+  return (
+    <View>
+      <Text>ScanInput</Text>
+    </View>
+  )
+}
+
+export default ScanInput
---
+++ screens/ScanOutput.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ScanOutput = () => {
+  return (
+    <View>
+      <Text>ScanOutput</Text>
+    </View>
+  )
+}
+
+export default ScanOutput
