77f07a1f3005653172b44215ac2ed4b2e04afec0
Lee <leejavaa@gmail.com>
2023-03-24 09:21:37 +0200

    UNT-1-Navigation base zero complete

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Signup" component={Signup} />
