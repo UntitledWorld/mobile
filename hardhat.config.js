028bf1ae016061c3cee60794cbbf457d44d34e2a
Lee <leejavaa@gmail.com>
2023-03-29 01:18:37 +0200

    UNT-10-Contract Deployed and Verified

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/JbuF4QM6bZPjNVhPxhSkdbsJg5slJBtj",
+      accounts : ["c247c9f624e1f133497fc9e173903dad7a9cfc12fbfcf8048e65c56e53fea02b"],
+    },
+  },
+  etherscan: {
+    apiKey: "8Q3SKYDK17P2G1XICZR68EUKWFN3MTB87E",
+  },
 };
