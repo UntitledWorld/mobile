35a6f16f621d6a29420d63bd4755d5c40a4b4737
Lee <leejavaa@gmail.com>
2023-09-20 08:23:44 +0200

    Deprecated

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,11 +6,11 @@ module.exports = {
   solidity: "0.8.17",
   networks: {
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/JbuF4QM6bZPjNVhPxhSkdbsJg5slJBtj",
-      accounts : ["c247c9f624e1f133497fc9e173903dad7a9cfc12fbfcf8048e65c56e53fea02b"],
+      url: "SEPOLIA_ALCHEMY_API",
+      accounts: ["SEPOLIA_ACCOUNT"],
     },
   },
   etherscan: {
-    apiKey: "8Q3SKYDK17P2G1XICZR68EUKWFN3MTB87E",
+    apiKey: "ETHERSCAN_API_KEY",
   },
 };
